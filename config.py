731de87a0e942e8a9ea48d37ce74c877184914d7
amirmnett <iamyelowguy@gmail.com>
2026-08-23 03:23:33 +0330

    Load environment variables from .env file

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", 0)) # آیدی عددی شما در تلگرام
