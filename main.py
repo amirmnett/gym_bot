acc55eff273964af9547b6cd5b044f0fa518e1ec
amirmnett <iamyelowguy@gmail.com>
2026-08-23 03:24:25 +0330

    Initialize bot with auth router and logging
    Set up bot with logging and routing for authentication.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+import config
+from handlers.auth import auth_router
+# فایل‌های دیگر هم بعداً اینجا ایمپورت می‌شوند مثل:
+# from handlers.admin import admin_router
 
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher()
+    
+    # اضافه کردن روترها (بخش‌های مختلف بات)
+    dp.include_router(auth_router)
+    # dp.include_router(admin_router)
+    
+    print("Bot is starting...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
